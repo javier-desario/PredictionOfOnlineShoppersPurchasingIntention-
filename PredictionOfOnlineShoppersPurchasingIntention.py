1dc58e677ce5b18fbae1698b88c6be46b61153a5
Javier <desarioj@hotmail.com>
2021-03-12 11:34:25 -0300

    Incluyo .py file con modelo updateado a la ipynb del master

--- PredictionOfOnlineShoppersPurchasingIntention.py
+++ PredictionOfOnlineShoppersPurchasingIntention.py
@@ -62,23 +62,31 @@
 # 
 #     revenue: Variable to be classified, 1 indicates that the visitor has bought and 0 indicates that the visitor has not bought.
 
-# In[4]:
+# In[1]:
 
-
-import re
-import datetime
 
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier, VotingClassifier, GradientBoostingClassifier, AdaBoostClassifier
+from sklearn.metrics import f1_score
+from sklearn.model_selection import GridSearchCV, cross_val_score, StratifiedKFold
+from sklearn.svm import SVC
+from sklearn.neural_network import MLPClassifier
+
 from pathlib import Path
 
 sns.set(style='dark', context='notebook', palette='plasma')
+random_state = 42
 
 
-# In[9]:
+# In[2]:
 
 
 path = Path('C:\\Users\\desar\\OneDrive\\Escritorio\\data_science\\Prediction of Online Shoppers Purchasing Intention')
@@ -87,7 +95,7 @@ train = pd.read_csv(path/'train_set.csv', index_col = 'id')
 test = pd.read_csv(path/'test_set.csv', index_col = 'id')
 
 
-# In[10]:
+# In[3]:
 
 
 train.head()
@@ -97,26 +105,25 @@ train.head()
 # 
 # El dataset ya esta limpio
 
-# In[11]:
+# In[4]:
 
 
-print(train.shape)
-print(test.shape)
+train.shape, test.shape
 
 
-# In[13]:
+# In[5]:
 
 
-print(train.describe())
+train.describe()
 
 
-# In[17]:
+# In[6]:
 
 
-print(train.info())
+train.info()
 
 
-# In[27]:
+# In[7]:
 
 
 corr_matrix = train.corr()
@@ -128,13 +135,13 @@ _= sns.heatmap(corr_matrix, cmap = 'viridis', annot = True)
 plt.show()
 
 
-# In[28]:
+# In[8]:
 
 
 corr_matrix['revenue'].sort_values()
 
 
-# In[105]:
+# In[9]:
 
 
 #Cut outliers for better visulization
@@ -154,7 +161,7 @@ _= plt.scatter(train_v.Administrative_Duration, train_v.ProductRelated_Duration,
                train_v.Informational_Duration, alpha = .3)
 
 
-# In[113]:
+# In[10]:
 
 
 #Swarmplot takes really long to run when many values overlap, make violinplot instead
@@ -170,7 +177,7 @@ _= sns.violinplot(train_v['revenue'], train_v['Informational_Duration'], hue =  
 plt.show()
 
 
-# In[125]:
+# In[11]:
 
 
 _= plt.figure(figsize = (10,5))
@@ -181,48 +188,298 @@ _= sns.barplot('Informational', 'Informational_Duration', hue = 'revenue', data 
 plt.show()
 
 
-# In[151]:
+# In[12]:
 
 
 _= sns.catplot('ProductRelated', 'ProductRelated_Duration', data=train_v, height=5, aspect=2)
-#_= plt.xticks(x, "")     #Delete x axis labels for cleannes
+_= plt.xticks([])     #Delete x axis labels for cleannes
 plt.show()
 
 
-# In[173]:
+# In[13]:
 
 
 _= sns.barplot(train.revenue, train.SpecialDay, train.Weekend)
 plt.show()
 
 
-# In[175]:
+# In[14]:
 
 
 _= plt.figure(figsize = (10,5))
 _= sns.violinplot(train.OperatingSystems, train.Region)
 
 
-# In[184]:
+# In[15]:
 
 
 _= plt.figure(figsize = (20,10))
-_= sns.barplot(train.revenue, train.PageValues, train.Month)
+order = ['Ene', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+_= sns.barplot(train.revenue, train.PageValues, train.Month, hue_order=order)
 
 
-# In[189]:
+# In[16]:
 
 
 _= plt.figure(figsize = (15,7))
 _= sns.scatterplot(train.BounceRates, train.ExitRates, train.revenue, alpha =.5)
 
 
-# In[199]:
+# In[17]:
+
+
+train.Month.unique()
+
+
+# In[18]:
 
 
 _= plt.figure(figsize = (10,5))
-_= sns.barplot('Month', 'revenue', data = train, ci = False)
+order = ['Ene', 'Feb', 'Mar', 'Apr', 'May', 'June', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+_= sns.barplot('Month', 'revenue', data = train, ci = False, order=order)
 plt.show()
 
 
-# JUEVES: VER DE SACAR ALGUN P-VALUE! A LA TARDE Primer modelo ya
+# In[19]:
+
+
+train.plot(kind='scatter', x='revenue', y='PageValues');
+
+
+# In[20]:
+
+
+_= plt.figure(figsize = (15,7))
+_= sns.scatterplot(train.ExitRates, train.PageValues, train.revenue, alpha =.5)
+
+
+# # Preprocessing
+
+# Preprocessing de las variables VisitorType, Weekend & Month
+
+# In[21]:
+
+
+train.head()
+
+
+# In[22]:
+
+
+train.VisitorType.unique()
+
+
+# In[23]:
+
+
+VisitorType_map = {'New_Visitor': 0, 'Returning_Visitor': 1, 'Other': 2}
+train.VisitorType = train.VisitorType.map(VisitorType_map)
+test.VisitorType = test.VisitorType.map(VisitorType_map)
+
+
+# In[24]:
+
+
+def WeekendMapping(x):
+    """Maps Weekend bool var to binary var"""
+    
+    if x:
+        return 1
+    else:
+        return 0
+
+for df in [train, test]:
+    df['WeekendOk'] = df.Weekend.apply(WeekendMapping)  
+    df.drop('Weekend', axis=1, inplace=True)
+
+
+# In[25]:
+
+
+ns = [i for i in range(len(train.Month.unique()))]
+months = train.Month.unique().tolist()
+
+MonthMapping = dict(zip(months, ns))
+for df in [train, test]:
+    df.Month = df.Month.map(MonthMapping)
+
+
+# In[26]:
+
+
+test.head()
+
+
+# In[27]:
+
+
+X = train.copy()
+X_test = test.copy()
+X.drop('revenue', axis=1, inplace=True)
+y = train['revenue']
+
+X_train, X_valid, y_train, y_valid = train_test_split(
+                                        X, y, test_size=0.2, 
+                                        random_state=random_state)
+
+print(X_train.shape, X_valid.shape, y_train.shape, y_valid.shape, X_test.shape)
+
+
+# # Modelling
+
+# In[28]:
+
+
+knc = KNeighborsClassifier()
+knc_model = knc.fit(X_train, y_train)
+y_pred = knc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[29]:
+
+
+logreg = LogisticRegression()
+logreg_model = logreg.fit(X_train, y_train)
+y_pred = logreg_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[30]:
+
+
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+lda = LinearDiscriminantAnalysis()
+lda_model = lda.fit(X_train, y_train)
+y_pred = lda.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[31]:
+
+
+svc = SVC(random_state=random_state, probability = True)
+svc_model = svc.fit(X_train, y_train)
+y_pred = svc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[32]:
+
+
+tree = DecisionTreeClassifier(random_state=random_state)
+tree_model = tree.fit(X_train, y_train)
+y_pred = tree_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[33]:
+
+
+forest = RandomForestClassifier(random_state=random_state)
+rfc_model = forest.fit(X_train, y_train)
+y_pred = rfc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[34]:
+
+
+etc = ExtraTreesClassifier(random_state=random_state)
+etc_model = forest.fit(X_train, y_train)
+y_pred = etc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[35]:
+
+
+gbc = GradientBoostingClassifier(random_state=random_state)
+gbc_model = gbc.fit(X_train, y_train)
+y_pred = gbc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[36]:
+
+
+abc = AdaBoostClassifier(random_state=random_state)
+abc_model = abc.fit(X_train, y_train)
+y_pred = abc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[37]:
+
+
+mlpc = MLPClassifier(random_state=random_state)
+mlpc_model = mlpc.fit(X_train, y_train)
+y_pred = mlpc_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[40]:
+
+
+#rf_param_grid = {"max_depth": [None],
+              #"max_features": [1, 7, 15],
+              #"min_samples_split": [2, 5, 15],
+              #"min_samples_leaf": [1, 3, 10],
+              #"bootstrap": [False],
+              #"n_estimators" :[100,500],
+              #"criterion": ["gini"]}
+
+#kfold = StratifiedKFold(n_splits=4)
+#gsRFC = GridSearchCV(forest, param_grid = rf_param_grid, cv=kfold, scoring="f1", n_jobs= -1, verbose = 1)
+#gsRFC.fit(X_train,y_train)
+#RFC_best = gsRFC.best_estimator_
+# Best score
+#gsRFC.best_score_
+
+#y_pred = gsRFC.predict(X_valid)
+#f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[44]:
+
+
+#gsRFC.best_params_
+
+
+# In[46]:
+
+
+votingC = VotingClassifier(
+    estimators=[('rfc', RandomForestClassifier(max_features=7,
+                                               min_samples_leaf=3,
+                                               min_samples_split=15,
+                                               n_estimators=500, 
+                                               random_state = random_state)),
+                ('extc', ExtraTreesClassifier(random_state = random_state)),                 
+                ('gbc', GradientBoostingClassifier(random_state = random_state))],
+    voting = 'hard', n_jobs = 6)
+
+voting_model = votingC.fit(X_train, y_train)
+y_pred = voting_model.predict(X_valid)
+
+f1_score(y_valid, y_pred, average = 'macro')
+
+
+# In[39]:
+
+
+sub = gbc_model.predict(X_test)
+submission = pd.DataFrame({'id': test.index, 'revenue': sub})
+submission.to_csv('submission.csv', index = False)
+
